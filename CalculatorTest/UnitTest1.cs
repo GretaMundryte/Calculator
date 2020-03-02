63653dc26d6d7d7108fb01a3faf7c138fac1f49c
Greta <greta@local>
2020-03-02 18:44:49 +0200

    Initial commit

---
+++ CalculatorTest/UnitTest1.cs
@@ -0,0 +1,52 @@
+using System;
+using NUnit.Framework;
+using Persons;
+
+namespace CalculatorTest
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void MultipliesNumbers()
+        {
+            var result = Calculator.Run(new [] {"multiply", "50", "5"});
+            
+            Assert.AreEqual(250, result);
+        }
+        
+        [Test]
+        public void DivideNumbers()
+        {
+            var result = Calculator.Run(new string[] {"divide", "50", "5"});
+            
+            Assert.AreEqual(10, result);
+        }
+
+        [Test]
+        public void SubtractNumber()
+        {
+            var result = Calculator.Run(new string[] {"subtract", "50", "5"});
+            
+            Assert.AreEqual(45, result);
+        }
+
+        [Test]
+        public void AddNumbers()
+        {
+            var result = Calculator.Run(new string[] {"sum", "50", "5"});
+            Assert.AreEqual(55, result);
+        }
+
+        [Test]
+        public void UnclearNumber()
+        {
+            var result = Calculator.Run(new string[] {"fail", "50", "5"});
+            Assert.AreEqual(0, result);
+        }
+    }
+}
---
+++ Persons/Calculator.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Persons
+{
+    public class Calculator
+    {
+        static void Main(string[] args)
+        {
+            var result = Run(args);
+            System.Console.WriteLine(result);
+        }
+
+        public static double Run(string[] args)
+        {
+            var a = args[1];
+            var b = args[2];
+            var firstNumber = Convert.ToDouble(a);
+            var secondNumber = Convert.ToDouble(b);
+
+            var action = args[0];
+
+            if (action == "multiply")
+            {
+                return firstNumber * secondNumber;
+            }
+            else if (action == "divide")
+            {
+                return firstNumber / secondNumber;
+            }
+            else if (action == "subtract")
+            {
+                return firstNumber - secondNumber;
+            }
+            else if (action == "sum")
+            {
+                return firstNumber + secondNumber;
+            }
+            else
+            {
+                return 0;
+            }
+        }
+    }
+}
